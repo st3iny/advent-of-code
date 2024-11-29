6d989ddbf29cfac211c13f51dc35626c164b2feb
Richard Steinmetz <richard@steinmetz.cloud>
2024-11-29 21:40:46 +0100

    Solve day 13 part 2 of 2023

--- 2023/day13/src/main.rs
+++ 2023/day13/src/main.rs
@@ -15,6 +15,18 @@ fn main() {
 }
 
 fn part1(input: &str) -> String {
+    solution(input, find_vertical, find_horizontal)
+}
+
+fn part2(input: &str) -> String {
+    solution(input, find_vertical_part2, find_horizontal_part2)
+}
+
+fn solution<V, H>(input: &str, find_vertical: V, find_horizontal: H) -> String
+where
+    V: Fn(&[String]) -> Option<usize>,
+    H: Fn(&[String]) -> Option<usize>,
+{
     let mut acc = 0;
 
     let mut fields = Vec::new();
@@ -26,7 +38,7 @@ fn part1(input: &str) -> String {
             continue;
         }
 
-        current_field.push(line);
+        current_field.push(line.to_string());
     }
     if !current_field.is_empty() {
         fields.push(current_field);
@@ -34,13 +46,11 @@ fn part1(input: &str) -> String {
 
     for field in &fields {
         if let Some(axis) = find_vertical(field) {
-            // println!("Found vertical axis between {} and {}", axis, axis + 1);
             acc += axis * 100;
             continue;
         }
 
         if let Some(axis) = find_horizontal(field) {
-            // println!("Found horizontal axis between {} and {}", axis, axis + 1);
             acc += axis;
         }
     }
@@ -48,7 +58,7 @@ fn part1(input: &str) -> String {
     acc.to_string()
 }
 
-fn find_vertical(field: &[impl PartialEq]) -> Option<usize> {
+fn find_vertical(field: &[String]) -> Option<usize> {
     for i in 1..field.len() {
         let mut found = true;
         let mut j = 0;
@@ -73,11 +83,11 @@ fn find_vertical(field: &[impl PartialEq]) -> Option<usize> {
     None
 }
 
-fn find_horizontal(field: &[&str]) -> Option<usize> {
+fn find_horizontal(field: &[String]) -> Option<usize> {
     find_vertical(&transpose(field))
 }
 
-fn transpose(field: &[&str]) -> Vec<String> {
+fn transpose(field: &[String]) -> Vec<String> {
     let mut out = Vec::new();
     let row_len = field.first().expect("Field to have at least one row").len();
     for x in 0..row_len {
@@ -95,8 +105,65 @@ fn transpose(field: &[&str]) -> Vec<String> {
     out
 }
 
-fn part2(input: &str) -> String {
-    todo!()
+fn find_vertical_part2(field: &[String]) -> Option<usize> {
+    // Don't accept axis from part 1 as there is at least one smudge to fix
+    let original_axis = find_vertical(field);
+    let new_axis = find_vertical_part2_rec(field, false, original_axis);
+    if original_axis.is_some() {
+        assert_ne!(original_axis, new_axis, "Should not find axis from part 1");
+    }
+
+    new_axis
+}
+
+fn find_vertical_part2_rec(
+    field: &[String],
+    smudge_fixed: bool,
+    original_axis: Option<usize>,
+) -> Option<usize> {
+    for i in 1..field.len() {
+        let mut found = true;
+        let mut j = 0;
+        while i - j > 0 {
+            let row1_index = i - j - 1;
+            match (field.get(row1_index), field.get(i + j)) {
+                (Some(row1), Some(row2)) if row1 != row2 => {
+                    if smudge_fixed || diff(row1, row2) > 1 {
+                        found = false;
+                        break;
+                    }
+
+                    let mut fixed_field = field.to_vec();
+                    fixed_field[row1_index] = row2.clone();
+                    if let Some(axis) = find_vertical_part2_rec(&fixed_field, true, original_axis) {
+                        return Some(axis);
+                    }
+                }
+                (None, _) | (_, None) => break,
+                _ => {}
+            }
+
+            j += 1;
+        }
+
+        if found && original_axis != Some(i) {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+fn diff(row1: &str, row2: &str) -> usize {
+    assert_eq!(row1.len(), row2.len());
+    row1.chars()
+        .zip(row2.chars())
+        .filter(|(a, b)| a != b)
+        .count()
+}
+
+fn find_horizontal_part2(field: &[String]) -> Option<usize> {
+    find_vertical_part2(&transpose(field))
 }
 
 #[cfg(test)]
@@ -110,10 +177,8 @@ mod tests {
         assert_eq!(part1(INPUT), include_str!("../part1.solution.txt").trim());
     }
 
-    /*
     #[test]
     fn test_part2() {
         assert_eq!(part2(INPUT), include_str!("../part2.solution.txt").trim());
     }
-    */
 }
